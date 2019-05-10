b41748c4779af471a2bd5394ad4e110aaf9a80b3
unknown <dasithkuruppu@gmail.com>
2019-05-10 06:51:13 +0530

    multiple fixes removed unnecessory packages, added typescript build essential .babelrc added .gitignore

--- handler.ts
+++ handler.ts
@@ -2,6 +2,6 @@ import { APIGatewayProxyEvent } from "aws-lambda";
 import { indexElasticSearch } from "./streams/process";
 
 
-export async function processStreams(event: APIGatewayProxyEvent) {
+export async function processStreams(event:APIGatewayProxyEvent) {
   await indexElasticSearch(event);
 }
--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
     ]
   },
   resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ]
+    extensions: ['.ts']
   },
   output: {
     libraryTarget: "commonjs2",
